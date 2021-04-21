f77e12d3febe93db9faaafaef2fc1292a71b9052
Muhammad Danish Bin Mohamad Ritzuan <18DDT19F1031@student.ptss.edu.my>
2021-04-21 11:37:09 +0800

    new update

--- Function and Sub/Form1.vb
+++ Function and Sub/Form1.vb
@@ -8,13 +8,12 @@
     End Sub
 
     Private Sub btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
-        Nama(TextBox1.Text)
+        Nama(strNama:=TextBox1.Text)
     End Sub
 
     Private Sub Nama(ByVal strNama As String)
-        Dim strShowNama As String
-        strShowNama = TextBox1.Text
-        Result2.Text = strShowNama
+
+        Result2.Text = strNama
     End Sub
 
     Function paparUmur()
@@ -27,13 +26,12 @@
     End Function
 
     Function umur(ByVal intUmur As Integer) As Integer
-        Dim intShowUmur As Integer
-        intShowUmur = TextBox2.Text
-        Result4.Text = intShowUmur
+
+        Result4.Text = intUmur
         Return umur
     End Function
 
     Private Function btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
-        umur(TextBox2.Text)
+        umur(intUmur:=TextBox2.Text)
     End Function
 End Class
