d2f3a571134964a1eab25ca4e094d6b533718635
Muhammad Danish Bin Mohamad Ritzuan <18DDT19F1031@student.ptss.edu.my>
2021-04-21 11:09:08 +0800

    new update function and sub

--- Function and Sub/Form1.vb
+++ Function and Sub/Form1.vb
@@ -8,32 +8,32 @@
     End Sub
 
     Private Sub btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
-        Dim strShowNama As String
-        strShowNama = TextBox1.Text
-        Result2.Text = strShowNama
         Nama(strNama:=TextBox1.Text)
     End Sub
 
-    Private Sub Nama(strNama As String)
-        Dim strInsert As String
+    Private Sub Nama(ByVal strNama As String)
+        Dim strShowNama As String
+        strShowNama = TextBox1.Text
+        Result2.Text = strShowNama
     End Sub
 
     Function paparUmur()
         Result3.Text = "Umur"
+        Return paparUmur
     End Function
 
     Private Function btn3_Click(sender As Object, e As EventArgs) Handles Btn3.Click
         paparUmur()
     End Function
 
-    Function umur(intUmur As Integer) As Integer
-        Dim intInsert As Integer
-    End Function
-
-    Private Function btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
+    Function umur(ByVal intUmur As Integer) As Integer
         Dim intShowUmur As Integer
         intShowUmur = TextBox2.Text
         Result4.Text = intShowUmur
+        Return umur
+    End Function
+
+    Private Function btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
         umur(intUmur:=TextBox2.Text)
     End Function
 End Class
